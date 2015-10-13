6a20e45f8702164f38fdb2a49971f5e4b7b52610
77120 <77120@excelsiorline.com>
2015-10-13 11:48:15 +0200

    Update keywordlinking.js

--- keywordlinking.js
+++ keywordlinking.js
@@ -4,31 +4,31 @@ TODO
 1. pre int not working with \b 
 2. allow html is not working yet
 3. load external json file
-4. performance!!!
 
 */
 
 (function() {
-//SETTINGS
+		/*Match Settings*/
 
-var skipcurlybraces = true,
-	skipsquarebraces = false,
-	casesensitive = false,
+
+	var skipcurlybraces = true,
+		skipsquarebraces = false,
+		casesensitive = false,
 		
-	/*Popover Settings*/
-	/*if popover set to true, bootstrap css and js is required */
-	enablepopover = true,
-	popover_trigger = 'hover',
-	popover_placement = 'right',
-	popover_allow_html = true,// must be on for now
-	/*if allow_images is true allow_html also must be true*/
-	popover_allow_images = true,
-	/*title mutation values capitaliseFirstLetter, capitaliseAll, none*/
-	title_mutation_settings = 'capitaliseFirstLetter';
+		/*Popover Settings*/
+		/*if popover set to true, bootstrap css and js is required */
+		enablepopover = true,
+		popover_trigger = 'hover',
+		popover_placement = 'right',
+		popover_allow_html = true,// must be on
+		/*if allow_images is true allow_html also must be true*/
+		popover_allow_images = true,
+		/*title mutation values capitaliseFirstLetter, capitaliseAll, none*/
+		title_mutation_settings = 'capitaliseFirstLetter';
 	
-// END SETTINGS
+	// END SETTINGS
 	
-	// load json file
+	// load a json file
 	var crosskeywords = 
 		[
 			{
@@ -77,21 +77,40 @@ var skipcurlybraces = true,
 	var precharacters = "\\b";
 	
 	var elements = document.getElementsByClassName('keywordlinking');
-	
+
+	var keywords = "";
+	crosskeywords.forEach(function(keyword, i) {
+	var alternative = i==0?"":"|";
+		keywords += alternative+keyword.crosslink_key;
+	});
+
+	casesensitive?regexpmod="g":regexpmod="gi";
+	var regexp = new RegExp(donotsearchintags+precharacters+keywords+postcharacters,regexpmod);
+
+
 	for(var i = 0; i < elements.length; i++){
-	
-		keyword = crosskeywords[i].crosslink_key;
-	    	crosskeywords.forEach(function(keyword) {
-			text = elements[i].innerHTML;
+		text = elements[i].innerHTML;
+
+		textnew = text.replace(regexp,function(match){
+			matched = getCrosslink(match)[0];
 			
-			enablepopover?popover="data-toggle='popover' imgurl='"+keyword.image+"' desc='"+keyword.desc+"' title='"+titleMutation(keyword.crosslink_key)+"'":popover="";
-			casesensitive?regexpmod="g":regexpmod="gi";
-			var regexp = new RegExp(donotsearchintags+precharacters+keyword.crosslink_key+postcharacters,regexpmod);
-			textnew = text.replace(regexp,function(match){return "<a href='"+keyword.url+"' "+popover+">"+match+"</a>"});
-			elements[i].innerHTML = textnew;
+			enablepopover?popover="data-toggle='popover_crosslink' imgurl='"+matched.image+"' desc='"+matched.desc+"' title='"+titleMutation(match)+"'":popover="";
+			return "<a href='"+matched.url+"' "+popover+">"+match+"</a>";
 		});
+		elements[i].innerHTML = textnew;
 	}
-	
+
+
+	function getCrosslink(match) {
+		match = match.toLowerCase();
+	  	return crosskeywords.filter(
+	     	function(crosskeywords){
+	     		return crosskeywords.crosslink_key == match
+	     	}
+	  	);
+	}
+
+
 	function titleMutation(string)
 	{
 		switch(title_mutation_settings) {
@@ -107,7 +126,7 @@ var skipcurlybraces = true,
 	}
 	
 	if (enablepopover){
-		$('[data-toggle="popover"]').popover({
+		$('[data-toggle="popover_crosslink"]').popover({
 	    	trigger: popover_trigger,
 	    	html: popover_allow_html,
 	    	placement: popover_placement,
